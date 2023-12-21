da4b43d8c136e5a45088ada317c0465002ecf91d
Piyush Sahu <pyushsahud123@gmail.com>
2023-12-21 20:05:33 +0530

    Header and Slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import './Home.css'
 import { EffectFade, Autoplay } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -15,19 +16,72 @@ export default function Home() {
             disableOnInteraction: false,
           }}
           modules={[EffectFade, Autoplay]}
-          className="mySwiper"
         >
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <div className="slide-block">
+              <div className="image">
+                <img src="/slider/1.jpg" />
+              </div>
+              <div className="overlay-block">
+                <div className="container">
+                  <div className="inner-block">
+                    <div className="title">
+                      Lorem ipsum dolor sit.
+                    </div>
+                    <div className="content">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quidem nam totam.
+                    </div>
+                    <div className="link-block">
+                      <Link to="/about-us">Learn More</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <div className="slide-block">
+              <div className="image">
+                <img src="/slider/2.jpg" />
+              </div>
+              <div className="overlay-block">
+                <div className="container">
+                  <div className="inner-block">
+                    <div className="title">
+                      Lorem ipsum dolor sit.
+                    </div>
+                    <div className="content">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quidem nam totam.
+                    </div>
+                    <div className="link-block">
+                      <Link to="/about-us">Learn More</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            <div className="slide-block">
+              <div className="image">
+                <img src="/slider/3.jpg" />
+              </div>
+              <div className="overlay-block">
+                <div className="container">
+                  <div className="inner-block">
+                    <div className="title">
+                      Lorem ipsum dolor sit.
+                    </div>
+                    <div className="content">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos quidem nam totam.
+                    </div>
+                    <div className="link-block">
+                      <Link to="/about-us">Learn More</Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
